2aa28c02befb433135221d32a6f9cde90e41de53
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-02-28 15:14:58 -0600

    can write to and read from database

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-const firebase = require("firebase");
+const firebase = require("firebase/app");
 // Required for side-effects
 require("firebase/firestore");
 
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-  apiKey: 'AIzaSyCMnizv4k6K4lhOQXnZPTjtgpEcqk1JS8M',
-  authDomain: 'react-firebase-todo-ec1d6.firebaseapp.com',
-  projectId: 'react-firebase-todo-ec1d6'
-});
 
-const db = firebase.firestore();
 
 function App() {
+  // Initialize Cloud Firestore through Firebase
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCMnizv4k6K4lhOQXnZPTjtgpEcqk1JS8M',
+    authDomain: 'react-firebase-todo-ec1d6.firebaseapp.com',
+    projectId: 'react-firebase-todo-ec1d6'
+  });
+
+  const db = firebase.firestore();
+
+  db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
+
+  // Add a second document with a generated ID.
+  db.collection("users").add({
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
+
+  db.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
